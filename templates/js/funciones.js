a18945993458fc7e8f9ba7613d8a091cd48ba0fe
Val <valeria.carvajal01@correo.usa.edu.co>
2021-03-18 19:04:37 -0500

    Funcion para mostrar nombre y saldo

--- templates/js/funciones.js
+++ templates/js/funciones.js
@@ -2,64 +2,43 @@
 function login(correo, contrasenia) {
     //Almacena los datos en JSON
     var obj = {
-        // "email": correo,
-        // "password": contrasenia
+
     }; //Envio de datos por AJAX 
     $.ajax({
-        /* method: 'POST',
-        url: url + '/ControladorLogin.py', //Ruta Especificada 
-        dataType: 'json', //Tipo de dato
-	data: obj,
-    	success: function(rta) {
-	console.log(rta)
-            response=JSON.parse(rta);
-            if(response.tipo==="OK"){
-               vista2(1)
-            }
-            else{
-                vista2(2)
-		//Envio de False
-            }
-        },*/
+
         error: function(response) {
             console.log(JSON.stringify(response))
         }
 
     });
 }
+
+
+//Mostrar nombre del usuario
+function mostrarNombre(nombre, apellido) {
+    $("#nombreUser").empty();
+    $("#nombreUser").append(nombre + ' ' + apellido)
+}
+
+//Mostrar saldo del usuario
+function mostrarSaldo(saldo) {
+    $("#saldoUser").empty();
+    $("#saldoUser").append(' ' + saldo)
+}
+
 
 // Registrar Usuario
 function crearCuenta(cedula, nombre, apellido, telefono, ocupacion, fecha, direccion, contrasenia) {
     //Almecena los datos en JSON
-    var obj = {
-        /*"email": correo,
-        "fname": nombre,
-        "lname": apellido,
-        "password": contrasenia,
-        "rol": 'Operario'*/
-    }; //Envio de datos por AJAX con metodo POST
+    var obj = {}; //Envio de datos por AJAX con metodo POST
     $.ajax({
-        /*
-                method: 'POST',
-                url: url + '/ControladorRegistro.py',
-                dataType: 'json',
-        	data: obj,
-                success: function(rta) {
-                    console.log(rta)
-                    response=JSON.parse(rta);
-                    console.log(response)
-                    if(response.tipo==="OK"){ //Autenticacion de tipo
-                        vista1(1)
-                    }else{
-                       vista1(2)
-                    }
-              },*/
         error: function(response) {
             console.log(JSON.stringify(response))
         }
     });
 }
 
+//Mostrar historial de Servicios
 function historial(buscar) {
     var obj = {
 
--- templates/js/publicitarOferta.js
+++ templates/js/publicitarOferta.js
@@ -1,77 +1,19 @@
-let boton = document.getElementById("icono");
-let enlaces = document.getElementById("enlaces");
-let contador = 0;
-$("#div-file").hide();
-$("#stock").hide();
-$("#TextoProducto").hide();
-$("#TextoServicio").show();
-$( "#ContactarButton" ).empty();
-$( "#ContactarButton" ).append("Seleccione");
-
-boton.addEventListener("click", function() {
-    if (contador == 0) {
-        enlaces.className = ('enlaces dos');
-        contador = 1;
-    } else {
-        enlaces.classList.remove('dos');
-        enlaces.className = ('enlaces uno');
-        contador = 0;
-    }
-})
-
-window.addEventListener('resize', function() {
-    if (screen.width > 750) {
-        contador = 0;
-        enlaces.classList.remove('dos');
-        enlaces.className = ('enlaces uno');
-
-    }
-})
-
-window.addEventListener('click', function(e) {
-    console.log(e.target);
-    if (contador == false) {
-        let span = document.querySelector('.links-header');
-        if (e.target == span) {
-            contador = 0;
-        }
-    }
-});
-
-
-//Enviar datos
-$("#Contactar").click(function() {
-
-    //Recolectar Datos
-    nombre = $("#nameC").val();
-    telefono = $("#telC").val();
-    correo = $("#emailC").val();
-    comentario = $("#comentC").val();
-    //Verificar datos
-    if (nombre == "" || telefono == "" || correo == "" || comentario == "") {
-        swal("Error", "Por favor, Ingrese todos los datos", "error");
-    } else {
-        swal("Correcto", "Gracias por contactarnos", "success")
-    }
-
-});
-
 $("#ButtonServicios").click(function() {
-    $( "#ContactarButton" ).empty();
-    $( "#ContactarButton" ).append("Servicio");
-        $("#div-file").hide();
-        $("#stock").hide();
-        $("#TextoProducto").hide();
-        $("#TextoServicio").show();
+    $("#ContactarButton").empty();
+    $("#ContactarButton").append("Servicio");
+    $("#div-file").hide();
+    $("#stock").hide();
+    $("#TextoProducto").hide();
+    $("#TextoServicio").show();
 
 });
 $("#ButtonProductos").click(function() {
-    $( "#ContactarButton" ).empty();
-    $( "#ContactarButton" ).append("Producto");
-        $("#div-file").show();
-        $("#stock").show();
-        $("#TextoProducto").show();
-        $("#TextoServicio").hide();
+    $("#ContactarButton").empty();
+    $("#ContactarButton").append("Producto");
+    $("#div-file").show();
+    $("#stock").show();
+    $("#TextoProducto").show();
+    $("#TextoServicio").hide();
 
 
 });
