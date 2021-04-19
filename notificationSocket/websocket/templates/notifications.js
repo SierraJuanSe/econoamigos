4657101d34645142e3d1a91a54c690d8d24ba8f5
Sebastian Sierra <juansebastiansierrac@gmail.com>
2021-04-18 19:54:34 -0500

    se deja archivo notifications.js con las funciones que se deben implementar en el frontend para escuchar las notificaciones

--- notificationSocket/websocket/templates/notifications.js
+++ notificationSocket/websocket/templates/notifications.js
@@ -1,176 +1,44 @@
-const URL = 'http://127.0.0.1:5000'
+const URLSocket = 'http://127.0.0.1:5000'
 let numNotifications = 0
 
-const socketCustomer = io('http://127.0.0.1:5000', {
-    autoConnect: false,
-})
-const socketVendor = io('http://127.0.0.1:5000', {
-    autoConnect: false,
+// Objeto socket que maneja la connection
+const socket = io(URLSocket, {
+  autoConnect: false,
 })
 
-const customerLogin = {
-    emailUsuario: 'comprador@mail.com',
-    passwordUsuario: 'compras123',
-}
-const vendorLogin = {
-    emailUsuario: 'vendedor@mail.com',
-    passwordUsuario: 'ventas123',
-}
-let customer = {
-    emailUsuario: 'comprador@mail.com',
-    passwordUsuario: 'compras123',
-}
-let vendor = {
-    emailUsuario: 'vendedor@mail.com',
-    passwordUsuario: 'ventas123',
-}
+/* 
+acciones
 
-let products = []
+estos son los eventos que envia el cliente al servidor para establecer la conexion
+y enviar informacion.
+*/
+
+/*
+Conecta el socket al servidor
+Esta funcion se debe poner cuando se confirma el login del usuario
+*/
+socket.connect() 
+
+/*
+Desconecta al socket del servidor
+Se debe llamar cuando el usaurio cierra sesion
+*/
+socket.disconnect()
 
 
-/* acciones */
+/* escuchas
 
-const setConnectedVendor = (connected) => {
-    $('#connectVendor__btn').prop('disabled', connected)
-    $('#disconnectVendor__btn').prop('disabled', !connected)
-    $('#product__container').toggle()
-}
+estos son los eventos que escuchan al servidor en busca de informacion
 
-const setConnectedCostumer = (connected) => {
-    $('#connectCustomer__btn').prop('disabled', connected)
-    $('#disconnectCustomer__btn').prop('disabled', !connected)
-}
-
-const connectVendor = () => {
-    fetch(URL + '/ingreso', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(vendorLogin),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            vendor = data.token
-            socketVendor.connect()
-            setConnectedVendor(true)
-        })
-}
-
-const connectCustomer = () => {
-    fetch(URL + '/ingreso', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(customerLogin),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            customer = data.token
-            socketCustomer.connect()
-            setConnectedCostumer(true)
-        })
-}
-
-const disconnectVendor = () => {
-    socketVendor.disconnect()
-    setConnectedVendor(false)
-}
-
-const disconnectCustomer = () => {
-    socketCustomer.disconnect()
-    setConnectedCostumer(false)
-}
-
-const getProducts = () => {
-    fetch(URL + '/consultarMisOfertas', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({"id":vendor.id}),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            products = data.info
-            d = data.info.map(item => {
-              return `<tr>
-                <td>${item.nombre}</td>
-                <td>${item.descripcion}</td>
-                <td><input 
-                  class='buy_btn' 
-                  type="button" 
-                  value=${item.id}
-                  onclick='buyProduct(${item.id})'></td></tr>`
-            })
-            $('#product_table').append(d)
-        })
-}
-
-const buyProduct = (value) => {
-  if(socketCustomer.connected){
-    p = products.filter(item => item.id === value)[0]
-    data = {
-      "idUsuario": customer.id,
-      "precio": p.precio,
-      "idOferta": p.id
-    }
-    fetch(URL + '/insertarCompra', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-  })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("compra", data)
-      })
-  }
-}
-
-/* ESCUCHAS DEL WEB-SOCKET Vendedor*/
-socketVendor.on('sid', (data) => {
-    vendor.sid = data.sid
-    window.vendorsid = data.sid
-    setCookie('vendorsid', vendorsid)
-    socketVendor.emit('userInfo', vendor)
+*/
+ 
+// El servidor envia un id unico confirmando su conexion
+socket.on('sid', (data) => {
+  socketVendor.emit('userInfo', readCookie('token'))
 })
 
-socketCustomer.on('buyNotification', (data) => {
-  console.log("costumer", data);
-})
-
+// escucha si se ha creado una compra de alguna de sus ofertas publicadas
 socketVendor.on('buyNotification', (data) => {
   numNotifications += 1;
-  info = `<p>Te hicieron una compra de: ${data.info.nombre} por $${data.info.precio}</p>`
-  $('#notification_list').append(info)
-  console.log("vendor:", data);
+  // aca se llama a la funcion que dibuja la notificacion
 })
-
-/* ESCUCHAS DEL WEB-SOCKET comprador*/
-socketCustomer.on('sid', (data) => {
-    customer.sid = data.sid
-    window.customersid = data.sid
-    setCookie('custumersid', customersid)
-    socketCustomer.emit('userInfo', customer)
-})
-
-$(() => {
-    $('form').on('submit', (e) => {
-        e.preventDefault()
-    })
-    $('#product__container').hide()
-    $('#connectVendor__btn').click(() => connectVendor())
-    $('#connectCustomer__btn').click(() => connectCustomer())
-    $('#disconnectVendor__btn').click(() => disconnectVendor())
-    $('#disconnectCustomer__btn').click(() => disconnectCustomer())
-    $('#getProducts__btn').click(() => getProducts())
-    $('.buy_btn').click((e) => buyProduct(e))
-})
-
-function setCookie(name, token) {
-    document.cookie =
-        `${name}=` + encodeURIComponent(token) + '; max-age=3600; path=/'
-}
