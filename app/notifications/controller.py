91750c92ef77b5d961c9b70c4b3d3016a09258ff
Sebastian Sierra <juansebastiansierrac@gmail.com>
2021-05-22 15:44:14 -0500

    event listeners para el chat

--- app/notifications/controller.py
+++ app/notifications/controller.py
@@ -1,5 +1,5 @@
-from flask import jsonify, request
-from flask_socketio import emit, rooms
+from flask import request
+from flask_socketio import emit, join_room, leave_room
 from app import socketio, onlineUsers
 
 @socketio.on('connect')
@@ -17,3 +17,33 @@ def disconnect():
 def userInfo(data):
   user = request.sid
   onlineUsers[user] = data
+
+
+@socketio.on('connect', namespace='/chat')
+def connect():
+  print('se conecto a chat')
+
+@socketio.on('disconnect', namespace='/chat')
+def disconnect():
+  print('se desconecto de chat')
+
+
+@socketio.on('join', namespace='/chat')
+def join(data):
+  data['newUser'] = request.sid
+  print('Se une a la sala')
+  join_room(data['room'])
+  emit('join', data, to=data['room'], namespace='/chat')
+
+@socketio.on('leave', namespace='/chat')
+def leave(data):
+  data['leaveUser'] = request.sid
+  print('Se va de la sala')
+  emit('leave', data, to=data['room'], namespace='/chat')
+  leave_room(data['room'])
+
+
+@socketio.on('message', namespace='/chat')
+def message(data):
+  print('Se une a la sala')
+  emit('message', data, to=data['room'], include_self=False, namespace='/chat')
--- notificationSocket/.flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=WebSocket.py
-FLASK_ENV=development
